3cbbd0630c7de00bf7945213364bbe2f0b4eca91
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2023-05-09 10:52:11 -0300

    Add files via upload

---
+++ exemplo01/teste.js
@@ -0,0 +1,33 @@
+var nome = prompt("Digite seu nome: ")
+
+console.log(nome)
+
+
+//alert("Bem vindo " + nome)
+
+/*/let a = 15
+
+if(true){
+    let a = 10
+    console.log(a)
+}
+
+
+console.log(a) */
+
+//calculando soma de 2 numeros
+
+var n1 = parseInt(prompt("Digite n1: "))
+
+var n2 = parseInt (prompt("Digite n2: "))
+
+var soma = n1+n2
+
+console.log("a soma é " + soma)
+
+console.log (typeof n1)
+
+//Template string
+
+//console.log(`A soma é ${soma}`)
+document.write(`<p> A soma é ${soma} </p>`)
